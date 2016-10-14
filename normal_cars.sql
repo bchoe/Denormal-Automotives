3feaeae7950ed3f887a280255d6a587e34f15e2d
Bryan Choe <bryan.choe88@gmail.com>
2016-10-14 12:20:48 -1000

    completed

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,28 +1,60 @@
---create make table
+/*DROP TABLE IF EXISTS car_model;
+DROP TABLE IF EXISTS car_make;
 
 CREATE TABLE car_make (
   P_id SERIAL NOT NULL,
-  code character varying(125),
-  title character varying(125),
+  code CHARACTER VARYING(125),
+  title CHARACTER VARYING(125),
   PRIMARY KEY (P_id)
 );
 
---create model table
-
 CREATE TABLE car_model (
-  F_id integer,
-  code character varying(125),
-  title character varying(125),
-  year integer,
+  F_id INTEGER,
+  code CHARACTER VARYING(125),
+  title CHARACTER VARYING(125),
+  year INTEGER,
   FOREIGN KEY (F_id) REFERENCES car_make(P_id)
 );
 
---insert data into car_make table
+INSERT INTO car_make(code, title)
+SELECT DISTINCT make_code, make_title
+FROM car_models
+ORDER BY make_title ASC;
+
+SELECT * FROM car_make;*/
 
 
+--------------------------------------------------
 
---insert data into car_model table
+DROP TABLE IF EXISTS make;
+DROP TABLE IF EXISTS models;
 
+CREATE TABLE make (
+  make_code CHARACTER VARYING(125) NOT NULL,
+  make_title CHARACTER VARYING(125) NOT NULL,
+  id SERIAL PRIMARY KEY
+);
 
+INSERT INTO make(make_code, make_title)
+SELECT DISTINCT make_code, make_title
+FROM car_models;
 
---Queries???!?!?!?!
+CREATE TABLE models (
+  id SERIAL PRIMARY KEY,
+  model_code CHARACTER VARYING(125) NOT NULL,
+  model_title CHARACTER VARYING(125) NOT NULL,
+  year integer NOT NULL,
+  make_id integer REFERENCES make (id)
+);
+
+INSERT INTO models(model_code, model_title, year, make_id)
+SELECT DISTINCT model_code, model_title, year, make_id
+FROM car_models INNER JOIN make ON (make.make_code = car_models.make_code);
+
+SELECT DISTINCT make_title FROM car_models;
+
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'VOLKS';
+
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+
+SELECT * FROM car_models WHERE year BETWEEN 2010 and 2015;
